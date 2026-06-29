87ab5061ceef05ff83c2da605dd2dd00bc4736e8
Pranshu Kumar <pranshu251205@gmail.com>
2026-06-29 11:18:51 +0530

    Phase 5 Milestone 1: Production MAX30102 driver and Wearable V3 packet architecture

--- firmware/xiao_nrf52840/src/parksense_wearable_v3/packet.h
+++ firmware/xiao_nrf52840/src/parksense_wearable_v3/packet.h
@@ -1,22 +1,52 @@
 #pragma once
 
+/*
+ * ==========================================================
+ * ParkinSense Wearable V3
+ * BLE Packet Definition
+ * ==========================================================
+ */
+
 #include <Arduino.h>
 
 #include "config.h"
 
-#pragma pack(push,1)
+#pragma pack(push, 1)
 
 /*
+ * ----------------------------------------------------------
  * Packet Header
+ * ----------------------------------------------------------
+ *
+ * version
+ *     Packet format version.
+ *
+ * flags
+ *     Sensor status flags.
+ *
+ * reserved
+ *     Reserved for future use.
+ *
+ * timestamp_us
+ *     Timestamp of the first sample.
+ *
  */
 
 struct PacketHeader
 {
+    uint8_t version;
+
+    uint8_t flags;
+
+    uint16_t reserved;
+
     uint32_t timestamp_us;
 };
 
 /*
- * One Sensor Sample
+ * ----------------------------------------------------------
+ * One Wearable Sample
+ * ----------------------------------------------------------
  */
 
 struct SensorSample
@@ -35,7 +65,9 @@ struct SensorSample
 };
 
 /*
- * BLE Packet
+ * ----------------------------------------------------------
+ * Complete BLE Packet
+ * ----------------------------------------------------------
  */
 
 struct WearablePacket
@@ -46,3 +78,36 @@ struct WearablePacket
 };
 
 #pragma pack(pop)
+
+/*
+ * ----------------------------------------------------------
+ * Packet Version
+ * ----------------------------------------------------------
+ */
+
+constexpr uint8_t PACKET_VERSION = 1;
+
+/*
+ * ----------------------------------------------------------
+ * Packet Flags
+ * ----------------------------------------------------------
+ */
+
+enum PacketFlags : uint8_t
+{
+    FLAG_IMU_VALID      = 1 << 0,
+
+    FLAG_PPG_VALID      = 1 << 1,
+
+    FLAG_FINGER_PRESENT = 1 << 2,
+
+    FLAG_BATTERY_LOW    = 1 << 3,
+
+    FLAG_RESERVED_4     = 1 << 4,
+
+    FLAG_RESERVED_5     = 1 << 5,
+
+    FLAG_RESERVED_6     = 1 << 6,
+
+    FLAG_RESERVED_7     = 1 << 7
+};
